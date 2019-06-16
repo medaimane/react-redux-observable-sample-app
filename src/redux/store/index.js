85608f5f5fec050d8b337d8111be3c01331026b9
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2019-06-16 23:00:51 +0100

    feat: setup  middleware to the store, epics

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,28 @@
-import {createStore} from 'redux';
-import rootReducer from '../reducers';
+import {applyMiddleware, createStore} from 'redux';
+import {createEpicMiddleware} from "redux-observable";
+import {createBrowserHistory} from "history";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {routerMiddleware} from 'connected-react-router';
+import createRootReducer from '../reducers';
+import rootEpic from '../epics';
 
-export default createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const epicMiddleware = createEpicMiddleware();
+
+export const history = createBrowserHistory();
+
+export default function configureStore() {
+ const store = createStore(
+     createRootReducer(history),
+     composeWithDevTools(
+        applyMiddleware(
+            routerMiddleware(history),
+            epicMiddleware
+        ),
+     ),
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
+
+ epicMiddleware.run(rootEpic);
+
+ return store;
+}
